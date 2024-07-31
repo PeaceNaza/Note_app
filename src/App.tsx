21a1fc2def9a9cbb9bc5d05a39a96be4b4fd9e2c
PeaceNaza <peacedonatus38@gmail.com>
2024-07-31 20:02:07 +0100

    chore: Add Vite configuration files, Prettier configuration, and gitignore file

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NewNote from "./components/NewNote";
+
+const App = () => {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>hello</h1>} />
+        <Route path="new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+};
+
+export default App;
